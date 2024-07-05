7ebc83482660b922bdccd7e43d27ba4b7886e9b9
piyushsingh0992 <piyushsingh0992@gmail.com>
2024-07-05 09:32:26 +0530

    changes in the UI

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -21,7 +21,7 @@ const Home: React.FC = () => {
   return (
     <div className="container mx-auto p-4">
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {movies.map((movie) => (
           <MovieCard key={movie._id} movie={movie} />
         ))}
