cc737d48d1006522a1c061fb7f873f8fecf5c098
piyushsingh0992 <piyushsingh0992@gmail.com>
2024-07-05 09:36:03 +0530

    changes

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your backend base URL
+  baseURL: process.env.REACT_APP_API_BASE_URL, 
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Add a request interceptor to include the token if available
+
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
